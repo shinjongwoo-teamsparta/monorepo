e819966c5f83712c94877eb2b0732fff8eff0af9
shinjw <jongwoo.shin@teamsparta.co>
2025-01-30 00:42:40 +0900

    fixup

--- packages/clients/client3/next.config.ts
+++ packages/clients/client3/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ["@teamsparta-japan/shared1"],
 };
 
 export default nextConfig;
