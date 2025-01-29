c829ce119ea6157388f0e33f73f36ded32277a40
shinjw <jongwoo.shin@teamsparta.co>
2025-01-29 21:17:01 +0900

    edit schema

--- packages/shared1/src/schema/user.ts
+++ packages/shared1/src/schema/user.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const userSchema = z.object({
   name: z.string(),
-  age: z.number().min(18, { message: "You must be at least 18 years old" }),
+  age: z.number().min(15, { message: "You must be at least 15 years old" }),
   timestamp: z.coerce.date(),
   email: z.string({ message: "Email is required" }).email({ message: "Invalid email address" }),
 });
