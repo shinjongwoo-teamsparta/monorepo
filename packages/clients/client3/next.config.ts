3425eeaf7b9f98f8a5d6edf321bb0e53ba41033f
shinjw <jongwoo.shin@teamsparta.co>
2025-01-29 23:38:21 +0900

    comment out next.js config

--- packages/clients/client3/next.config.ts
+++ packages/clients/client3/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ["@teamsparta-japan/shared1"],
+  // transpilePackages: ["@teamsparta-japan/shared1"],
 };
 
 export default nextConfig;
