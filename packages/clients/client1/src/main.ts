d8a14769753fc1b0079622561b98860f5b4f0b08
shinjw <jongwoo.shin@teamsparta.co>
2025-01-29 14:03:09 +0900

    edit npm script

--- packages/clients/client1/src/main.ts
+++ packages/clients/client1/src/main.ts
@@ -1,8 +1,6 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
-import { range } from 'lodash'
 import ky from 'ky'
 import { User } from '@teamsparta-japan/shared1'
 
